8d618c5828c300e3332ec2bdd4c41139aada26fc
Danish <danishriz100@gmail.comgit commitexit>
2020-10-23 12:56:16 -0600

    Gave Proper inheritance to classes

--- OOP Shapes/OOP Shapes/Shapes/Circle.cs
+++ OOP Shapes/OOP Shapes/Shapes/Circle.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace OOP_Shapes.Shapes
 {
-    class Circle
+    class Circle : Shape
     {
     }
 }
--- OOP Shapes/OOP Shapes/Shapes/Rectangle.cs
+++ OOP Shapes/OOP Shapes/Shapes/Rectangle.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace OOP_Shapes.Shapes
 {
-    class Rectangle
+    class Rectangle : Shape
     {
     }
 }
