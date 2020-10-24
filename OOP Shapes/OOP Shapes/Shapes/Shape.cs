25b59795840884edf1290a34ef528eb7d6e9dda6
Danish <danishriz100@gmail.comgit commitexit>
2020-10-24 16:29:13 -0600

    Shape Class Properties and Method added

--- OOP Shapes/OOP Shapes/Shapes/Shape.cs
+++ OOP Shapes/OOP Shapes/Shapes/Shape.cs
@@ -6,5 +6,23 @@ namespace OOP_Shapes.Shapes
 {
     abstract class Shape
     {
+
+        // Public getter that is is overridden by derived class
+        public abstract double Area { get; }
+        public abstract double Perimeter { get; }
+        
+        //Traditional Property
+        public string Color { get; set; }
+
+        //defult constructor 
+        protected Shape()
+        {
+            Color = "White";
+        }
+
+        protected Shape(string color)
+        {
+            Color = color;
+        }
     }
 }
